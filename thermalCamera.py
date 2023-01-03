65063d7ffababdcc2f8ca2a86f3a51468151ecdd
Robert Page <rob@srcapture.com>
2023-01-03 21:06:23 +0000

    various fixes

--- thermalCamera.py
+++ thermalCamera.py
@@ -9,7 +9,7 @@ import adafruit_mlx90640
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
 mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ # set refresh rate
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # set refresh rate
 
 frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
 while True:
--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var root = document.getElementById("root");
+/*var root = document.getElementById("root");
 var table = document.createElement("table");
 for (var y = 0; y < 24; y++) {
     var tr = document.createElement("tr");
-    for (var x = 0; x < 32; x++) {
+    for (var x = 31; x >= 0; x--) {
         var td = document.createElement("td");
         td.id = "pixel_".concat(y * 32 + x);
         tr.appendChild(td);
@@ -12,28 +12,54 @@ for (var y = 0; y < 24; y++) {
     table.appendChild(tr);
 }
 
-root.appendChild(table);
+root.appendChild(table);*/
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/cameraHub").build();
 
 connection.on("ReceiveFrame", function (frame) {
 
-    const min = Number(frame.min);
+    let min = Number(frame.min);
     let max = Number(frame.max);
 
     if (max - 1 < min) {
         max++;
     }
 
+    const autoTemp = document.getElementById("tempAuto");
+
+    if (autoTemp.checked == false) {
+	min = document.getElementById("tempMin").value;
+	max = document.getElementById("tempMax").value;
+    }
+
+const canvas = document.getElementById('canvas');
+	const ctx = canvas.getContext('2d');
+
+	const newPixels = [];
+
     frame.pixels.forEach(function (pixel, idx) {
 
         const temp = Number(pixel);
         const normalised = Math.floor(255 * (temp - min) / (max - min));
-        const pixelElement = document.getElementById("pixel_".concat(idx));
         const bgColor = "rgb(" + normalised.toString() + ", 0, " + (255 - normalised).toString() + ")";
-        pixelElement.style.backgroundColor = bgColor;
-        pixelElement.textContent = temp.toFixed(1);
-    });
+
+	    newPixels.push(normalised);
+	    newPixels.push(0);
+	    newPixels.push(255-normalised);
+	    newPixels.push(255);
+	});	
+	     const imgData = new ImageData( new Uint8ClampedArray( newPixels ), 32, 24 );
+	     ctx.putImageData( imgData, 0, 0 );
+	     ctx.globalCompositeOperation = "copy";
+	     ctx.scale( 20, 20 ); 
+
+	    // newest browsers can control interpolation quality
+	     ctx.imageSmoothingQuality = "high";
+	     ctx.drawImage( canvas, 0, 0 );
+	    //
+	    // clean
+	     ctx.globalCompositeOperation = "source-over";
+	     ctx.setTransform( 1, 0, 0, 1, 0, 0);
 });
 connection.start()["catch"](function (err) {
     return console.error(err.toString());
